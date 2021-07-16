098161573f8a150eff35d27757c3b5378d9d88a4
Jaekeun Lee <ljg960730@naver.com>
2021-07-16 22:36:42 +0900

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS messages;
-CREATE TABLE messages
+CREATE TABLE `messages`
 (
-    id           INT(11)                       NOT NULL AUTO_INCREMENT,
-    text         VARCHAR(128) collate utf8_bin NOT NULL DEFAULT '',
-    created_date DATETIME                      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+    `id`           INT(11)                       NOT NULL AUTO_INCREMENT,
+    `text`         VARCHAR(128) collate utf8_bin NOT NULL DEFAULT '',
+    `created_date` DATETIME                      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 ) ENGINE InnoDB
   DEFAULT charset = utf8
   collate = utf8_bin
